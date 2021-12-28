52af10abbf2dfbfd04185c4fbfbb4f23e06850e7
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-28 14:59:17 +0900

    Initial Commit

---
+++ Shared/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
